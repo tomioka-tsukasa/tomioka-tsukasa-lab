bd20be610cf47015c4d9449c75db04fd34d28885
tomioka-tsukasa <tsukasa.creator@gmail.com>
2025-10-24 16:49:47 +0900

    feat: effect/glitch/sample-01: initialVisible 初期値調整

--- src/app/effect/glitch/components/Testor/Testor.tsx
+++ src/app/effect/glitch/components/Testor/Testor.tsx
@@ -2,11 +2,13 @@
 
 import { TestorPanel, WebGLProvider, GlitchSettings, useWebGL } from '@/components/TestorPanel'
 import { useEffect } from 'react'
+import { usePathname } from 'next/navigation'
 import { webglCtrl } from '../../modules/webgl/setupMember'
 import * as styles from './Testor.css'
 
 const TestorContent = () => {
   const { setImageChangerNoiseCtrl } = useWebGL()
+  const pathname = usePathname()
 
   useEffect(() => {
     // WebGL初期化を監視してContextに登録
@@ -25,11 +27,14 @@ const TestorContent = () => {
     console.log('Glitch triggered with settings:', settings)
   }
 
+  // sample-01 ページの場合は非表示
+  const isOnSample01 = pathname?.includes('/effect/glitch/sample-01')
+
   return (
     <TestorPanel
       effectType='glitch'
       onGlitchTrigger={handleGlitchTrigger}
-      initialVisible={true}
+      initialVisible={!isOnSample01}
     />
   )
 }
